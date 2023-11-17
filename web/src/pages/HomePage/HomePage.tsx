164afcff3c9d5accbd4ea5ceabd77d179e9176e6
Christopher Mickle <cmickle.dev@gmail.com>
2023-11-17 14:58:36 -0600

    bug fix for non-existing routes

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,9 @@
 import { MetaTags } from '@redwoodjs/web'
 
-import ArticlesCell from 'src/components/ArticlesCell'
-
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      <ArticlesCell />
     </>
   )
 }
