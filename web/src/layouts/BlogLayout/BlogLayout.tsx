a3cabe6201e2ace94318f42ec43c2c797e87c3d4
Christopher Mickle <cmickle.dev@gmail.com>
2023-11-17 13:37:47 -0600

    Add Post model and update routes and layout

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -3,14 +3,19 @@ import { Link, routes } from '@redwoodjs/router'
 type BlogLayoutProps = {
   children?: React.ReactNode
 }
-
+l
 const BlogLayout = ({ children }: BlogLayoutProps) => {
   return (
     <>
       <header>
-        <h1>Redwood Blog</h1>
+        <h1>
+          <Link to={routes.home()}>Redwood Blog</Link>
+        </h1>
         <nav>
           <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
             <li>
               <Link to={routes.about()}>About</Link>
             </li>
